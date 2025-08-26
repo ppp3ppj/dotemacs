a75e1b05916da21944cd79856d8d224de101f908
ppp <104762988+ppp3ppj@users.noreply.github.com>
2025-08-26 15:13:26 +0700

    Configure org-agenda-files for Linux and Windows
    Added conditional setting for org-agenda-files based on OS.

--- .emacs.rc/org-mode-rc.el
+++ .emacs.rc/org-mode-rc.el
@@ -1,4 +1,13 @@
 (global-set-key (kbd "C-x a") 'org-agenda)
 (global-set-key (kbd "C-c C-x j") #'org-clock-jump-to-current-clock)
 
-(setq org-agenda-files (list "~/Documents/Agenda/"))
+;; Linux or Windows for org file 
+(cond
+ ((eq system-type 'gnu/linux)
+  (setq org-agenda-files '("~/Documents/Agenda/")))
+
+ ((eq system-type 'windows-nt)
+  (setq org-agenda-files
+        (list (expand-file-name "Documents/Agenda/" (getenv "USERPROFILE")))))
+)
+;; (setq org-agenda-files (list "~/Documents/Agenda/"))
